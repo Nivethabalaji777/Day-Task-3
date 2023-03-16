3948f7b8bfdfda2db90da5119e6d781c2702ad7a
Nivetha T <127500611+Nivethabalaji777@users.noreply.github.com>
2023-03-16 19:58:05 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,57 @@
+let myResume={
+    "basics": {
+        "id":{
+      "name": "NIVETHA T",
+      "email": "nivethabharathi77@gamil.com",
+      "phone": 9677876677,
+      "degree": "B.Tech",
+        },
+    
+      "location": {
+        "address": "28/10A, Azhagamman koil street, Vadiveeswaram",
+        "postalCode": 629002,
+        "city": "Nagercoil",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "linkedin": "https://www.linkedin.com/in/nivetha-t-865b95253/",
+          "github":"https://github.com/Nivethabalaji777"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "ESSAY INFOTECH",
+        "position": "TRAINER & OFFICE ADMINISTRATOR",
+        "working Period": "2019 to 2022",
+        "summary": "Trained the students in computer on office automation, photoshop & typewriting. Managed payroll & benefits for 30+ employees including solving any related problems.",
+      },
+      {
+        "company": "ANNAMALAI ALUMINIUM & GLASS HOUSE",
+        "position": "CREATIVE DESIGNER",
+        "working Period": "2016 to 2017",
+        "summary": "Create unique designs with Photoshop & Coral draw. Successfully designed for Glasses & other print materials, Advertisements, Brochures & Logos.",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Kalasalingam University",
+        "department": "ECE",
+        "batch passed out": 2015,
+        "gpa": 7.2,
+      }
+    ],
+    "skills": [
+      {
+        "name": "HTML, CSS, Javascript, Photoshop, TypeWriting",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+  }
+  console.log(myResume);
